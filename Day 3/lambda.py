fb2535a804b2aaa34a6e93437f1cc19c7e950a1c
Your Name <your.email@example.com>
2026-09-01 19:33:52 +0530

    3rd day

---
+++ Day 3/lambda.py
@@ -0,0 +1,7 @@
+# Anonymous / Lambda Function
+# Definition
+
+# An anonymous function is a function that does not have a name. In Python, it is created using the lambda keyword.
+square = lambda x: x * x
+
+print(square(5))
