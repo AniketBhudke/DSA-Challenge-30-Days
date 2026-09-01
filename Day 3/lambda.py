4b2e5193fa262772a2c9f53c4766f0fc53689643
Your Name <bhudkea@gmail.com>
2026-09-01 20:07:32 +0530

    Complete Day 3 lambda functions exercise

--- Day 3/lambda.py
+++ Day 3/lambda.py
@@ -15,3 +15,21 @@ numbers = [1, 2, 3, 4, 5, 6]
 even = list(filter(lambda x: x % 2 == 0, numbers))
 
 print(even)
+
+# Anonymous / Lambda Function
+# Definition
+
+# An anonymous function is a function that does not have a name. In Python, it is created using the lambda keyword.
+square = lambda x: x * x
+
+print(square(5))
+
+# filter() Function
+# Definition
+
+# The filter() function is used to select elements from a list for which a given condition is True.
+
+numbers = [1, 2, 3, 4, 5, 6]
+even = list(filter(lambda x: x % 2 == 0, numbers))
+
+print(even)
