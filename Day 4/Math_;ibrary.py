ea245fc8081ba5bd2b393f7d28fdb4c43c9307f7
Your Name <bhudkea@gmail.com>
2026-09-02 19:12:27 +0530

    day 3rd

---
+++ Day 4/Math_;ibrary.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.sqrt(25))      # Square root → 5.0
+print(math.pow(2, 3))      # Power → 8.0
+print(math.factorial(5))  # Factorial → 120
+print(math.ceil(4.2))     # Round up → 5
+print(math.floor(4.8))    # Round down → 4
---
+++ Day 4/frozenset.py
@@ -0,0 +1,2 @@
+a=frozenset([2,3,4])
+print(a)
